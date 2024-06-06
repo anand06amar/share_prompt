f962e8a5c73df4ac4277e433279ffddd57ca74eb
anand06amar <anand07amar@gmail.com>
2024-06-06 23:43:38 +0530

    done with the file setup

---
+++ app/page.jsx
@@ -0,0 +1,20 @@
+
+
+const Home = () => {
+  return (
+    <section className="w-full flex-centre flex-col">
+        <h1 className="head_text text-center">
+            Discover & Share
+            <br className="max-md:hidden"/>
+            <span className="orange_gradient text-center">
+                AI-Powered Prompts
+            </span>
+        </h1>
+        <p className="des text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa a doloribus aliquam nobis et expedita suscipit, quo reiciendis minus maxime perspiciatis illum, magnam fuga, quam iure. Rem inventore minima voluptates.
+        </p>
+    </section>
+  )
+}
+
+export default Home
